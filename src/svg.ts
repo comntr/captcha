e68ee45d4c908ced86b5db8c44f8402b411e6c42
comntr <>
2019-07-26 20:48:17 -0700

    fuzzy svg

--- src/svg.ts
+++ src/svg.ts
@@ -1,4 +1,5 @@
 import * as glyphs from './glyphs';
+import * as config from './config';
 
 export function render(text: string): string {
   let w = text.length * 4 - 1;
@@ -15,16 +16,27 @@ export function render(text: string): string {
       if (!g[j]) continue;
       let x = dx + j % 3;
       let y = dy + (j / 3 | 0);
-      pts.push({x, y});
+      pts.push({ x, y });
     }
   }
 
   return `
     <svg viewBox="0 0 ${w} ${h}" xmlns="http://www.w3.org/2000/svg">
-      ${pts.map(p => `
-        <rect x="${p.x}" y="${p.y}"
-          width="1" height="1"
-          fill="#000"></rect>
-        `).join('')}
+      ${pts.map(p => svgrect(p.x, p.y)).join('')}
     </svg>`;
+}
+
+function rand(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
+function svgrect(x: number, y: number): string {
+  let rs = config.RECT_SIZE;
+  let rx = x + rand(0, 1 - rs);
+  let ry = y + rand(0, 1 - rs);
+
+  return `
+    <rect x="${rx}" y="${ry}"
+      width="${rs}" height="${rs}"
+      fill="#000"></rect>`;
 }
