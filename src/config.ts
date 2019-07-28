719781c64d0018b2853bd774dae4f2625c1e7261
comntr <>
2019-07-28 00:17:16 -0700

    Allow /question/...?ts=...

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const RECT_SIZE = 0.75;
 export const CERT_DIR = '/etc/letsencrypt/archive/comntr.live/';
 export const CERT_KEY_FILE = 'privkey1.pem';
 export const CERT_FILE = 'cert1.pem';
-export const GET_QUESTION_PATTERN = /^\/question\/(.+)$/;
-export const GET_POSTMARK_PATTERN = /^\/postmark\/(.+)\?answer=(.+)$/;
+export const GET_QUESTION_PATTERN = /^\/question\/(\w+)/; // ?ts=2514 is allowed
+export const GET_POSTMARK_PATTERN = /^\/postmark\/(\w+)\?answer=(\w+)$/;
 export const KEYS_FILE = '/etc/comntr/captcha/keys.json';
 export const PUBKEY_PREFIX = 7;
